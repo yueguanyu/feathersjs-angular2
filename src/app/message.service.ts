e4f0a73f67ae034ee4bfe767a24a7a036de995a1
Aurelien Giraud <aure@posteo.de>
2017-01-02 20:38:26 +0000

    Add info for dummy user if no info in message

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -5,7 +5,14 @@ import { RestService, SocketService } from './feathers.service';
 export class MessageService {
   private socket;
   private rest;
-  debugString = "Hi, from Message Service!";
+  
+  // A placeholder image if the user does not have one
+  private PLACEHOLDER = 'https://placeimg.com/60/60/people';
+  // An anonymous user if the message does not have that information
+  const dummyUser = {
+    avatar: PLACEHOLDER,
+    email: 'Anonymous'
+  }
 
   constructor(
     private socketService: SocketService,
