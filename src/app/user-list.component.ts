a54ad8c263d91cd7b7a996dcd4babb15aeb623c5
Aurelien Giraud <aure@posteo.de>
2017-01-03 20:03:01 +0000

    Fix usage of service in component

--- src/app/user-list.component.ts
+++ src/app/user-list.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { UserService } from './user.service'
 
 @Component({
   selector: 'app-user-list',
   template: `
             <ul class="flex flex-column flex-1 list-unstyled user-list">
-              <li *ngFor="let user of users">
-                {{user}}
+              <li *ngFor="let user of userService.users">
+                {{user.email}}
               </li>
             </ul>
   `
 })
 export class UserListComponent implements OnInit {
-  public users = ["Bob","Luke","Harry"];
-  
-  constructor() { }
+
+  constructor(private userService: UserService) {}
 
   ngOnInit() {
   }
